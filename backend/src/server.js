4c03093c3745956a648efb3340b8f80dcf7681c5
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-10 00:57:00 -0300

    Usando cors com express para poder fazer frontend e backend se comunicar

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import { router } from './routers/routes.mjs';
 import connectionDatabase from './db/database.mjs';
@@ -8,6 +9,8 @@ connectionDatabase();
 const PORT = process.env.PORT || 3333;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
