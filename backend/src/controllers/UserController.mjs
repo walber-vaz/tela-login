2c588cea1b35f10c0e6293b3853467796c91ba49
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-10 08:31:29 -0300

    Reafatorando o nome response e request para res/req

--- backend/src/controllers/UserController.mjs
+++ backend/src/controllers/UserController.mjs
@@ -4,20 +4,20 @@ import crypto from 'crypto';
 import User from '../models/Users.mjs';
 
 export default {
-  async index(request, response) {
+  async index(req, res) {
     try {
       const users = await User.find();
-      return response.status(200).json({ users });
+      return res.status(200).json({ users });
     } catch (error) {
-      return response.status(500).json({ error: error.message });
+      return res.status(500).json({ error: error.message });
     }
   },
 
-  async create(request, response) {
-    const { username, lastname, email, password } = request.body;
+  async create(req, res) {
+    const { username, lastname, email, password } = req.body;
 
     if (!username || !lastname || !email || !password) {
-      return response.status(400).json({ error: 'Missing data' });
+      return res.status(400).json({ error: 'Missing data' });
     }
 
     const user = new User({
@@ -30,38 +30,38 @@ export default {
 
     try {
       await user.save();
-      return response.status(201).json({ message: 'User created' });
+      return res.status(201).json({ message: 'User created' });
     } catch (error) {
-      return response.status(400).json({ error: error.message });
+      return res.status(400).json({ error: error.message });
     }
   },
 
-  async update(request, response) {
-    const { username, lastname, email, password } = request.body;
+  async update(req, res) {
+    const { username, lastname, email, password } = req.body;
 
     if (!username && !lastname && !email && !password) {
-      return response.status(400).json({ error: 'Missing data' });
+      return res.status(400).json({ error: 'Missing data' });
     }
 
-    if (username) response.user.username = username;
-    if (lastname) response.user.lastname = lastname;
-    if (email) response.user.email = email;
-    if (password) response.user.password = crypto.createHash('sha256').update(password).digest('hex');
+    if (username) res.user.username = username;
+    if (lastname) res.user.lastname = lastname;
+    if (email) res.user.email = email;
+    if (password) res.user.password = crypto.createHash('sha256').update(password).digest('hex');
 
     try {
-      await response.user.save();
-      return response.status(200).json({ message: 'User updated' });
+      await res.user.save();
+      return res.status(200).json({ message: 'User updated' });
     } catch (error) {
-      return response.status(500).json({ error: error.message });
+      return res.status(500).json({ error: error.message });
     }
   },
 
-  async delete(request, response) {
+  async delete(req, res) {
     try {
-      await response.user.remove();
-      return response.status(200).json({ message: 'User deleted' });
+      await res.user.remove();
+      return res.status(200).json({ message: 'User deleted' });
     } catch (error) {
-      return response.status(500).json({ error: error.message });
+      return res.status(500).json({ error: error.message });
     }
   }
 }
--- backend/src/middlewares/UserMiddlewares.mjs
+++ backend/src/middlewares/UserMiddlewares.mjs
@@ -2,21 +2,21 @@ import { validate as isUuid } from 'uuid';
 import User from '../models/Users.mjs';
 
 export default {
-  async validateId(request, response, next) {
-    const { id } = request.params;
+  async validateId(req, res, next) {
+    const { id } = req.params;
 
     if (!isUuid(id)) {
-      return response.status(400).json({ error: 'Invalid id' });
+      return res.status(400).json({ error: 'Invalid id' });
     }
 
     try {
       const user = await User.findById(id);
-      response.user = user;
+      res.user = user;
       if (!user) {
-        return response.status(404).json({ error: 'User not found' });
+        return res.status(404).json({ error: 'User not found' });
       }
     } catch (error) {
-      return response.status(500).json({ error: error.message });
+      return res.status(500).json({ error: error.message });
     }
 
     next();
